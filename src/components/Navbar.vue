258ae123925e4fac11c75d48200b6b76026a2410
sarahw607 <sarahw607@gmail.com>
2019-06-17 22:48:07 +0100

    Navigation and Home components

---
+++ src/components/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <nav>
+    <ul>
+      <li>
+        <a :to="{name: 'Home'}">Search</a>
+      </li>
+      <li>
+        <a href="#">Store Cupboard</a>
+      </li>
+      <li>
+        <a href="#">Food Advice</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+nav{
+  width: 100%;
+  background-color:#9A879D;
+  height: 50px;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+  text-align: center;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+</style>
