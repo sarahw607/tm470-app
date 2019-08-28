dbd0386c5f979360832d909296f3ad63df1796e5
Sarah Redway <sarah.redway@shields.energy>
2019-08-28 15:13:20 +0100

    food safety page

--- src/components/SafetyPage.vue
+++ src/components/SafetyPage.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="content">
     <h1>Food Safety Advice</h1>
+    <p>
+      While our application can be used to search for any recipe you may want, our main aim is to give you advice about food safety regarding use-by dates,
+      and to encourage you to use foods that would otherwise be thrown away. While some foods should not be consumed beyond their use-by dates, there are others that
+      can be safely consumed.*
+    </p>
+    <h2>Do's</h2>
+    <h3>Hard Cheese</h3>
+    <p></p>
+    <p
+      class="safety-message"
+    >*Food past use-by date is consumed at users' own risk, this page is purely informational and not instructive</p>
   </div>
 </template>
 
@@ -9,3 +20,13 @@ export default {
   name: 'SafetyPage'
 }
 </script>
+
+<style scoped>
+.safety-message {
+  font-size: 0.7em;
+}
+.content {
+  align-items: flex-start;
+  text-align: left;
+}
+</style>
